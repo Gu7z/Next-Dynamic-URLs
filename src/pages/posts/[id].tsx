ceafe8f6a6dc4affaa9bbca2174b4b38f705d174
Gustavo Ferri <gustavoferri13@gmail.com>
2023-11-24 16:43:21 -0400

    Basic nextjs with routing

---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,25 @@
+import { Metadata, ResolvingMetadata } from "next";
+import { useRouter } from "next/router";
+
+export const metadata: Metadata = {
+  title: "Post",
+  description: "Page for post",
+};
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return {
+    title: params.id,
+    description: `Página para o post $${params.id}`,
+  };
+}
+
+export default function Post() {
+  const router = useRouter();
+
+  return <p>Post: {router.query.id}</p>;
+}
